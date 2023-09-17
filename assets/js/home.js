e67bf0e0066f6c4072bfb4c1ce649c3949dcb992
Ajeya5999 <ajeyasengar@gmail.com>
2023-09-18 00:37:46 +0530

    fifth commit: added the favroites page

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,8 +3,9 @@
 let searchBar = document.getElementById("search-bar");
 let searchList = document.getElementById("search-list");
 let searchButton = document.getElementById("search-button");
-let pageHeader = document.getElementById("page-header");
-let content = document.getElementById("content");
+const pageHeader = document.getElementById("page-header");
+const content = document.getElementById("content");
+const viewFavButton = document.getElementById("view-favroites");
 
 //iife function
 
@@ -22,6 +23,8 @@ searchButton.addEventListener('click', function(event) {
 });
 
 pageHeader.addEventListener('click', showHome);
+
+viewFavButton.addEventListener('click', viewFavroites);
 
 // Functions
 
@@ -57,6 +60,7 @@ async function displayList() {
         let heroFavButton = document.createElement("button");
         heroFavButton.setAttribute('class', "hero-item-fav-button");
         heroFavButton.innerText = "Favroite";
+        heroFavButton.addEventListener('click', addFromHomeToFavList)
         heroButtonContainer.append(heroInfoButton, heroFavButton);
         heroItem.append(heroImage, heroName, heroButtonContainer);
         searchList.append(heroItem);
@@ -94,18 +98,23 @@ function removeContent() {
     }
 }
 
+function addFromHomeToFavList() {
+    localStorage.setItem(this.parentNode.parentNode.heroId, this.parentNode.parentNode.childNodes[1].innerText);
+    alert("added to Favroites");
+}
+
 // SuperHero Page
 
 async function showHeroInfo() {
     let heroId = this.parentNode.parentNode.heroId;
     removeContent();
-    let res = await fetch(`https://gateway.marvel.com:443/v1/public/characters/${heroId}?limit=100&ts=1&apikey=045f3b6f0feb72129d011e0c8c2da774&hash=e50120b2312ba2aeb9686d8ccfdcba4d`);
+    let res = await fetch(`https://gateway.marvel.com:443/v1/public/characters/${heroId}?ts=1&apikey=045f3b6f0feb72129d011e0c8c2da774&hash=e50120b2312ba2aeb9686d8ccfdcba4d`);
     let heroData = await res.json();
     let hero = heroData.data.results;
     hero = hero[0];
-    console.log(hero);
     let heroContainer = document.createElement("div");
     heroContainer.setAttribute("id", "hero-container");
+    heroContainer.heroId = heroId;
     let heroNameContainer = document.createElement('h1');
     heroNameContainer.setAttribute('id', 'hero-name-container');
     heroNameContainer.innerText = hero.name;
@@ -138,7 +147,12 @@ async function showHeroInfo() {
     heroSeries.append(heroSeriesHeader, heroSeriesList);
     heroEventsContainer.append(heroComics, heroSeries);
     heroDcrptContainer.append(heroIntroContainer, heroEventsContainer);
-    heroContainer.append(heroNameContainer, heroImgContainer, heroDcrptContainer);
+    let heroFavButton = document.createElement("button");
+    heroFavButton.setAttribute('class', "hero-item-fav-button");
+    heroFavButton.setAttribute('id', "learn-more-fav-button");
+    heroFavButton.innerText = "Favroite";
+    heroFavButton.addEventListener('click', addFromInfoToFavList);
+    heroContainer.append(heroNameContainer, heroImgContainer, heroDcrptContainer, heroFavButton);
     content.append(heroContainer);
 }
 
@@ -148,4 +162,59 @@ function fillHeroEvents(heroEvents, heroEventContainer) {
         event.innerText = heroEvents[iterator].name;
         heroEventContainer.append(event);
     }
+}
+
+function addFromInfoToFavList() {
+    localStorage.setItem(this.parentNode.heroId, this.parentNode.childNodes[0].innerText);
+    alert("added to Favroites");
+}
+
+// Favroites page
+
+async function viewFavroites() {
+    removeContent();
+    if(localStorage.length === 0) {
+        let message = document.createElement("h1");
+        message.setAttribute("id", "message");
+        message.innerText = "You have no favroites";
+        content.append(message);
+    }
+    else {
+        let favList = document.createElement("ul");
+        favList.setAttribute('id', 'fav-list');
+        for(let iterator = 0; iterator < localStorage.length; iterator++) {
+            let res = await fetch(`https://gateway.marvel.com:443/v1/public/characters/${localStorage.key(iterator)}?ts=1&apikey=045f3b6f0feb72129d011e0c8c2da774&hash=e50120b2312ba2aeb9686d8ccfdcba4d`);
+            let heroData = await res.json();
+            let hero = heroData.data.results;
+            hero = hero[0];
+            let heroItem = document.createElement("li");
+            heroItem.setAttribute('class', "hero-item");
+            heroItem.heroId = hero.id;
+            let heroImage = document.createElement("img");
+            heroImage.setAttribute('src', hero.thumbnail.path + "." + hero.thumbnail.extension);
+            heroImage.setAttribute('class', "hero-item-img");
+            let heroName = document.createElement("span");
+            heroName.setAttribute('class', "hero-item-name");
+            heroName.innerText = hero.name;
+            let heroButtonContainer = document.createElement("div");
+            heroButtonContainer.setAttribute('class', "hero-item-button-container");
+            let heroInfoButton = document.createElement("button");
+            heroInfoButton.setAttribute('class', "hero-item-info-button");
+            heroInfoButton.innerText = "Learn More"
+            heroInfoButton.addEventListener('click', showHeroInfo);
+            let removeFavButton = document.createElement("button");
+            removeFavButton.setAttribute('class', "remove-fav-hero-button");
+            removeFavButton.innerText = "Remove";
+            removeFavButton.addEventListener('click', removeFromFav)
+            heroButtonContainer.append(heroInfoButton, removeFavButton);
+            heroItem.append(heroImage, heroName, heroButtonContainer);
+            favList.append(heroItem);
+        }
+        content.append(favList);
+    }
+}
+
+function removeFromFav() {
+    localStorage.removeItem(this.parentNode.parentNode.heroId);
+    this.parentNode.parentNode.remove();
 }
